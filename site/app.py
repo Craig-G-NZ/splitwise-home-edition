6278fd8274171538f3cabaccb196f546e07374a4
Craig <92700720+Craig-G-NZ@users.noreply.github.com>
2023-11-26 21:49:53 +1300

    Update app.py

--- site/app.py
+++ site/app.py
@@ -168,4 +168,5 @@ def delete_expense(index):
     return render_template('delete.html', expense=expense)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
+
